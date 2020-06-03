edffed9978b921ca4a0dc53d18a3c04f69231792
Unknown <avinashjaiswal642@gmail.com>
2020-06-03 22:22:50 +0530

    code refactor and contract mod

--- client/src/app/services/doctor.service.ts
+++ client/src/app/services/doctor.service.ts
@@ -65,22 +65,22 @@ export class DoctorService {
     return this.http.post<any>(url,JSON.parse(JSON.stringify(newObj)),{headers:headers});
   }
 
-  // addPatient( user : User) : Observable<User>{
-  //   let url = this.baseUrl + "addPatient";
-  //   return this.http.post<User>(url,user).pipe(
-  //     tap(
-  //       resp => {
-  //         console.log("doc service -> addPatient : "+JSON.stringify(resp));
-  //       }
-  //     )
-  //   );
-  // }
-
   getHistory( args:any) : Observable<any>  {
     let url = this.baseUrl + "getHistoryForPatient";
     let headers=new HttpHeaders();
     headers.set('Content-Type','application/json');
     return this.http.post(url,JSON.parse(JSON.stringify(args)),{headers});
+  }
+
+  addPatientToDoctorList(){
+    let patientId=localStorage.getItem('patientId');
+    let url = this.baseUrl + "addPatientToDoctorList";
+    let headers=new HttpHeaders();
+    let newObj={"patientId":"","doctorId":""}
+    newObj.patientId=patientId;
+    newObj.doctorId=localStorage.getItem('doctorId');
+    headers.set('Content-Type','application/json');
+    return this.http.post<any>(url,JSON.parse(JSON.stringify(newObj)),{headers:headers});
   }
 
   isLoggedIn(){
